68786b8bffcea8fb5e7414de804dfb3c8f91a221
Gashanas <gashanas@gmail.com>
2018-03-21 00:18:32 +0200

    temp commit

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,17 +1,41 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { ExampleFormComponent } from './example-form/example-form.component';
 
 @NgModule({
-  imports: [
-    BrowserModule
-  ],
-  bootstrap: [
-    AppComponent
-  ],
-  declarations: [
-    AppComponent
-  ]
+    imports: [
+        BrowserModule,
+        CommonModule
+    ],
+    declarations: [
+        AppComponent,
+        ExampleFormComponent
+    ],
+    bootstrap: [
+        AppComponent,
+        ExampleFormComponent
+    ]
 })
 export class AppModule {}
+
+
+// import { NgModule } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
+//
+// import { AppComponent } from './app.component';
+//
+// @NgModule({
+//   imports: [
+//     BrowserModule
+//   ],
+//   bootstrap: [
+//     AppComponent
+//   ],
+//   declarations: [
+//     AppComponent
+//   ]
+// })
+// export class AppModule {}
